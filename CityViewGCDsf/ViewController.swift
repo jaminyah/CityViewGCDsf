7efce74ad16c70a4edc71b1ac07695d05c251405
jaminyah <jaminyah@gmail.com>
2017-11-28 19:28:43 -0500

    Populated UITableView with city name

--- CityViewGCDsf/ViewController.swift
+++ CityViewGCDsf/ViewController.swift
@@ -14,30 +14,48 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // Create URLSession that uses a delegate
     private lazy var session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
-    var receivedData: NSData?
     let url = URL(string: "http://cdn.jaminya.com/json/cities.json")!
+    var jsonObjects: NSArray?
+    
+    @IBOutlet weak var cityTableView: UITableView!
     
     
-    // Local data to populate UITableView
-    private let mountains = ["Denali", "Mount Logan", "Pico de Orizaba", "Mount Saint Elias", "Popocatépetl", "Mount Foraker",
-                           "Mount Lucania", "Iztaccíhuatl", "King Peak", "Mount Bona", "Mount Steele", "Cofre de Perote", "Mount Sanford",
-                           "Volcán Tajumulco", "Grand Teton", "Mount Slaggard", "Nevado de Toluca", "Nevado de Colima", "La Malinche", "Castle Peak"]
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
 
         let task = session.dataTask(with: url) { (data, response, error) in
             if error != nil {
                 print(error!.localizedDescription)
             } else {
-                self.receivedData = data as NSData?
                 
                 // Display received data in console debugger
                 if let data = data {
-                    if let jsonData = String(data: data, encoding: String.Encoding.utf8)
-                    {
-                        print(jsonData)
+                    
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                        if let major_cities = json["major_cities"] as? NSArray {
+                            self.jsonObjects = major_cities
+                            
+                            // print city objects in console debugger
+                            print(self.jsonObjects!)
+                            
+                            DispatchQueue.main.async {
+                                // Refresh table view
+                                self.cityTableView.reloadData()
+                            }
+                        }
+                    
+                        // Display to console debugger
+                       /* if let jsonData = String(data: data, encoding: String.Encoding.utf8)
+                        {
+                            print(jsonData)
+                            print(self.jsonObjects!)
+                        }*/
+                        
+                    } catch let error as NSError {
+                        print("Failed to parse: \(error.localizedDescription)")
                     }
                 }
             }
@@ -51,12 +69,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
 
-    
     // MARK:-
     // MARK: Table View Data Source Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mountains.count
+       /* if self.jsonObjects != nil {
+            return self.jsonObjects!.count
+        } else {
+            return 15
+        }*/
+        if let cities = self.jsonObjects {
+            return cities.count
+        } else {
+            return 15   // arbitrary value
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -67,7 +93,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 reuseIdentifier: cellIdentifier
             )
         }
-        cell?.textLabel?.text = mountains[indexPath.row]
+        
+       // if self.jsonObjects != nil {
+        if let cityObject = self.jsonObjects?[indexPath.row] as? NSDictionary {
+
+            print("cityObject: \(cityObject)")
+            cell?.textLabel?.text = cityObject["city"] as? String
+
+        } else {
+            cell?.textLabel?.text = "city_name"
+
+        }
+        
         return cell!
     }    
 }
